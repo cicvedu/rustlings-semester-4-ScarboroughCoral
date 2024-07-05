842d6955cd55d15471a8b51bcdf3015fab57485e
Coral <3249977074@qq.com>
2024-07-05 14:32:55 +0000

    finish to hashmap1

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,10 +5,13 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    // TODO: implement the message variant types based on their usage below]
+    ChangeColor(u8, u8, u8),
+        Echo(String),
+        Move(Point),
+        Quit,
 }
 
 struct Point {
@@ -40,6 +43,13 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message
+        use Message::*;
+        match message {
+            ChangeColor(r,g,b) => self.color=(r,g,b),
+            Echo(s) => self.message = s,
+            Move(p) => self.position = p,
+            Quit => self.quit = true,
+        }
         // variants
         // Remember: When passing a tuple as a function argument, you'll need
         // extra parentheses: fn function((t, u, p, l, e))
